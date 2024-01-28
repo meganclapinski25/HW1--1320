1c2904b5ce010d74b935e07e0aa0ed709d373a0b
meganclapinski25 <142531799+meganclapinski25@users.noreply.github.com>
2024-01-28 09:01:25 -0800

    #

--- index.js
+++ index.js
@@ -1,37 +1,124 @@
 const canvas = document.getElementById("myCanvas");
-      const ctx = canvas.getContext("2d");
-      let x = canvas.width / 2;
-      let y = canvas.height - 30;
-      let dx = 2;
-      let dy = -2;
+const ctx = canvas.getContext("2d");
+//constants   
       const ballRadius = 10;
       const paddleHeight = 10;
       const paddleWidth = 75;
-      let rightPressed = false;
-      let leftPressed = false;
-      let paddleX = (canvas.width- paddleWidth) / 2; 
       const brickRowCount = 7;
       const brickColumnCount = 12;
       const brickWidth = 55;
-        const brickHeight = 10;
-        const brickPadding = 10;
-        const brickOffsetTop = 30;
-        const brickOffsetLeft = 30;
-        let score = 0;
-        let lives = 3;
-        var bricks = [];
+      const brickHeight = 10;
+      const brickPadding = 10;
+      const brickOffsetTop = 30;
+      const brickOffsetLeft = 30;
+       
+//variables
+      
 
+      
+      
+      
+      let rightPressed = false;
+      let leftPressed = false;
+      let paddleX = (canvas.width- paddleWidth) / 2;
+      
+      let score = 0;
+      let lives = 3;
+
+//classes
+        class Sprite {
+            constructor(x = 0, y = 0, width = 100, height = 100, color = '#f00') {
+            this.x = x
+            this.y = y
+            this.width = width
+            this.height = height
+            this.color = color
+            }
+            render(ctx) {
+                ctx.beginPath();
+                ctx.rect(this.x, this.y, this.width, this.height);
+                ctx.fillStyle = this.color;
+                ctx.fill();
+                ctx.closePath();
+              }
+            
+        }
+        
+        export default Sprite
+
+        class Brick extends Sprite {
+            constructor(x, y, width = 75, height = 20, color = '#0095DD') {
+              super(x, y, width, height, color) // pass arguments to Sprite!
+              this.status = true // adds a new property
+            }
+          }
+        
+        class Ball extends Sprite {
+            constructor(x = 0, y = 0, radius = 10, color = "#0095DD") {
+              super(x, y, 0, 0, color)
+              this.radius = radius;
+              this.dx = 2
+              this.dy = -2
+            }
+          
+            move() {
+              this.x += this.dx
+              this.y += this.dy
+            }
+          
+            render(ctx) { // Overrides the existing render method!
+              ctx.beginPath();
+              ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+              ctx.fillStyle = this.color;
+              ctx.fill();
+              ctx.closePath();
+            }
+          }
+          const b = new Ball(200, 200, 10)
+          b.render(ctx)
+        
+
+        class Paddle extends Sprite {
+            constructor(x, y, width = 75, height = 20, color = '#0095DD') {
+            super(x, y, width, height, color)
+        }
+        move(leftPressed, rightPressed, canvasWidth){
+
+        }
+    }
+
+        class Background extends Sprite{
+            constructor(x, y, width, height, color = '#ffffff') {
+            super(x, y, width, height, color)
+            }
+        }
+        class Score extends Sprite{
+
+        }
+        class Lives extends Sprite{
+
+        }
+
+
+
+
+
+
+
+
+      
+ //brick array     
+      var bricks = [];
 
         for (var c = 0; c < brickColumnCount; c++) {
             bricks[c] = [];
             
             for (var r = 0; r < brickRowCount; r++) {
-                bricks[c][r] = { x: 0, y: 0, status: 1 };
+                bricks[c][r] = new Brick(0, 0);
             }
         }
-      document.addEventListener("keydown", keyDownHandler, false);
-    document.addEventListener("keyup", keyUpHandler, false);
-    document.addEventListener("mousemove", mouseMoveHandler, false);
+
+//functions
     function mouseMoveHandler(e){
         const relativeX = e.clientX - canvas.offsetLeft;
         if (relativeX > 0 && relativeX < canvas.width){
@@ -71,67 +158,20 @@ const canvas = document.getElementById("myCanvas");
             }   
         }
     }
-    function drawScore(){
-        ctx.font = "16px Arial";
-        ctx.fillstyle = "#0095DD";
-        ctx.fillText(`Score:${score}`, 8, 20);
-    }
-    function drawLives(){
-        ctx.font = "16px Arial";
-        ctx.fillStyle = "#c99bd1";
-        ctx.fillText(`Lives: ${lives}`, canvas.width -65,20);
-    }
+    
 
-      
-      function drawBall() {
-        ctx.beginPath();
-        ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-        
-        ctx.fillStyle = "#c99bd1";
-        ctx.fill();
-        ctx.closePath();
-       }
-       function drawPaddle() {
-        ctx.beginPath();
-        ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-        ctx.fillStyle = "#1a7011";
-        ctx.fill();
-        ctx.closePath();
-       }
-        function drawBricks() {
-            for (var c = 0; c < brickColumnCount; c++) {
-                for (var r = 0; r < brickRowCount; r++) {
-                    if (bricks[c][r].status === 1) {
-                        var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-                        var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-                        bricks[c][r].x = brickX;
-                        bricks[c][r].y = brickY;
-                        
-                        
-                        
-                        
+    
+       
 
 
-                    }
-                    if (r % 1 === 0 && c % 3 === 0){
-                        ctx.fillStyle = 'red'
-                    }else{
-                        ctx.fillStyle = 'blue'
-                    }
-
-                    ctx.beginPath();
-                    ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                        
-                    ctx.fill();
-                    ctx.closePath();
-                    
-                }
-            }
-        }
 
 
        
+    
 
+
+       
+//game function
        function draw() {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             drawBricks();
@@ -179,4 +219,10 @@ const canvas = document.getElementById("myCanvas");
             requestAnimationFrame(draw);
 
         }
-        draw();
+        //event listeners
+        document.addEventListener("keydown", keyDownHandler, false);
+        document.addEventListener("keyup", keyUpHandler, false);
+        document.addEventListener("mousemove", mouseMoveHandler, false);
+    
+    
+    draw();
